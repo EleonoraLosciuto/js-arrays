1c7cebb75370d6ca0daa09aa784afe48a3780fe4
Eleonora Losciuto <eleonora.losciuto@gmail.com>
2026-05-15 22:21:29 +0200

    01-arrays - exercises 5 to 8

--- arrays/01-arrays/script.js
+++ arrays/01-arrays/script.js
@@ -19,21 +19,28 @@ teachers.splice(4, 1, "Patrick"); // Delete elements starting from index 4, dele
 console.log(teachers);
 
 // 3. Rimuovi l'ultimo insegnante dall'array e salvalo nella variabile lastTeacher qui sotto
-const lastTeacher = teachers.pop();
+const lastTeacher = teachers.pop(); // To remove last item I use pop()
 console.log(lastTeacher, teachers);
 
 // 4. Rimuovi il primo insegnante dall'array e salvalo nella variabile firstTeacher qui sotto
-const firstTeacher = teachers.shift();
+const firstTeacher = teachers.shift(); // To remove first teacher I use shift()
 console.log(firstTeacher, teachers);
 
 // 5. Aggiungi un insegnante di nome 'Vanessa' alla fine dell'array teachers
+teachers.push("Vanessa"); // to add a new string at the end of the array I use push()
+console.log(teachers);
 
 // 6. Aggiungi un insegnante di nome 'Sarah' all'inizio dell'array teachers
+teachers.unshift("Sarah"); // to add a new string at the start of the array I use unshift()
+console.log(teachers);
 
 // 7. Trova l'indice dell'insegnante 'Lewis' nell'array teachers
 // e salvalo nella variabile lewisIndex
-const lewisIndex = null;
+const lewisIndex = teachers.indexOf("Lewis"); // to find the index of a string I use indexOf()
+console.log(lewisIndex);
 
 // 8. Verifica se l'array teachers è vuoto
 // e salva il risultato nella variabile isTeachersEmpty
-const isTeachersEmpty = null;
+const isTeachersEmpty = teachers.length;
+if (isTeachersEmpty > 0) console.log(`l'array non é vuoto: contiene ${isTeachersEmpty} elementi`);
+else console.log(`l'array é vuoto`);
